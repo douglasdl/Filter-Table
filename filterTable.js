a668d4494d2fbad3ac5c31156152bcf6097cbd42
Douglas Dias Leal <douglas_san@hotmai.com>
2020-11-27 10:37:16 +0900

    Expanded the filter to both columns.

--- filterTable.js
+++ filterTable.js
@@ -1,16 +1,22 @@
 function filterTable() {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td0, td1, i, txtValue0, txtValue1;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
+      td0 = tr[i].getElementsByTagName("td")[0];
+      td1 = tr[i].getElementsByTagName("td")[1];
+      if (td0 || td1) {
+        txtValue0 = td0.textContent || td0.innerText;
+        txtValue1 = td1.textContent || td1.innerText;
+        // If > -1 means the string was found
+        console.log(tr[i]);
+        if ((txtValue0.toUpperCase().indexOf(filter) > -1) || (txtValue1.toUpperCase().indexOf(filter) > -1)) {
+            // Keep row visible
+            tr[i].style.display = "";
         } else {
+            // Hide row
           tr[i].style.display = "none";
         }
       }
